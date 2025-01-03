045d4d58b86e6dde6825666603fdc49042c2e1b1
deacon <soydeacon@gmail.com>
2025-01-02 22:02:56 -0800

    feat(init): project setup

---
+++ trading-gamified/lint-staged.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  '*.{js,jsx,ts,tsx}': ['eslint --fix --no-warn-ignored'],
+  '**/*.ts?(x)': () => 'npm run check-types',
+};
---
+++ trading-gamified/next.config.mjs
@@ -0,0 +1,46 @@
+import { fileURLToPath } from 'node:url';
+
+import withBundleAnalyzer from '@next/bundle-analyzer';
+import { withSentryConfig } from '@sentry/nextjs';
+import createJiti from 'jiti';
+import withNextIntl from 'next-intl/plugin';
+
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+jiti('./src/libs/Env');
+
+const withNextIntlConfig = withNextIntl('./src/libs/i18n.ts');
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  nx: {
+    svgr: false,
+  },
+  eslint: {
+    dirs: ['.'],
+  },
+  poweredByHeader: false,
+  reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['@electric-sql/pglite'],
+  },
+};
+
+export default withSentryConfig(
+  bundleAnalyzer(withNextIntlConfig(nextConfig)),
+  {
+    org: 'trading-gamified',
+    project: 'trading-gamified',
+    silent: !process.env.CI,
+    widenClientFileUpload: true,
+    tunnelRoute: '/monitoring',
+    hideSourceMaps: true,
+    disableLogger: true,
+    automaticVercelMonitors: true,
+    telemetry: false,
+  },
+);
